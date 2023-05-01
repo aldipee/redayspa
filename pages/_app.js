91bf578bcde8a423186375c98dc7e0b8313b7e51
IvanYapeter <ivanyapeter@gmail.com>
2023-05-01 12:22:56 +0800

    add google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,36 @@
 import { useEffect } from 'react';
 import AOS from 'aos';
+import Script from 'next/script';
 
 import 'aos/dist/aos.css';
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     AOS.init();
   }, []);
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+          
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
