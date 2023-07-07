70b3148e7945914cb5089fcd3191c1b626b5269c
IvanYapeter <ivanyapeter@gmail.com>
2023-07-07 10:40:59 +0800

    remove error text

--- pages/faq.js
+++ pages/faq.js
@@ -104,18 +104,7 @@ export default function Home() {
                       data-bs-parent="#accordionExample"
                     >
                       <div className="accordion-body">
-                        <strong>
-                          This is the first item's accordion body.
-                        </strong>{" "}
-                        It is shown by default, until the collapse plugin adds
-                        the appropriate classes that we use to style each
-                        element. These classes control the overall appearance,
-                        as well as the showing and hiding via CSS transitions.
-                        You can modify any of this with custom CSS or overriding
-                        our default variables. It's also worth noting that just
-                        about any HTML can go within the{" "}
-                        <code>.accordion-body</code>, though the transition does
-                        limit overflow.
+                        dasdasd
                       </div>
                     </div>
                   </div>
@@ -139,18 +128,7 @@ export default function Home() {
                       data-bs-parent="#accordionExample"
                     >
                       <div className="accordion-body">
-                        <strong>
-                          This is the second item's accordion body.
-                        </strong>{" "}
-                        It is hidden by default, until the collapse plugin adds
-                        the appropriate classes that we use to style each
-                        element. These classes control the overall appearance,
-                        as well as the showing and hiding via CSS transitions.
-                        You can modify any of this with custom CSS or overriding
-                        our default variables. It's also worth noting that just
-                        about any HTML can go within the{" "}
-                        <code>.accordion-body</code>, though the transition does
-                        limit overflow.
+                        adasdq
                       </div>
                     </div>
                   </div>
@@ -174,18 +152,7 @@ export default function Home() {
                       data-bs-parent="#accordionExample"
                     >
                       <div className="accordion-body">
-                        <strong>
-                          This is the third item's accordion body.
-                        </strong>{" "}
-                        It is hidden by default, until the collapse plugin adds
-                        the appropriate classes that we use to style each
-                        element. These classes control the overall appearance,
-                        as well as the showing and hiding via CSS transitions.
-                        You can modify any of this with custom CSS or overriding
-                        our default variables. It's also worth noting that just
-                        about any HTML can go within the{" "}
-                        <code>.accordion-body</code>, though the transition does
-                        limit overflow.
+                        adasdq
                       </div>
                     </div>
                   </div>
