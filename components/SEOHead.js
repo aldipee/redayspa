8dc6fb87143d060ed129a59b840710582f3f5323
IvanYapeter <ivanyapeter@gmail.com>
2025-06-11 14:55:05 +0800

    split component footer on faq page

---
+++ components/SEOHead.js
@@ -0,0 +1,71 @@
+import Head from "next/head";
+
+export default function SEOHead({
+  title,
+  description,
+  keywords,
+  author,
+  ogTitle,
+  ogType = "website",
+  ogUrl,
+  ogImage,
+  ogDescription,
+  ogSiteName = "Re Massage Studio",
+  canonicalUrl,
+  noindex = false,
+  nofollow = false,
+}) {
+  const robots = `${noindex ? "noindex" : "index"},${
+    nofollow ? "nofollow" : "follow"
+  }`;
+
+  return (
+    <Head>
+      <meta charSet="utf-8" />
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta name="author" content={author || "Re Massage Studio"} />
+      <meta name="robots" content={robots} />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, user-scalable=no"
+      />
+
+      {/* Canonical URL */}
+      {canonicalUrl && <link rel="canonical" href={canonicalUrl} />}
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:title" content={ogTitle || title} />
+      <meta property="og:type" content={ogType} />
+      <meta property="og:url" content={ogUrl || canonicalUrl} />
+      <meta property="og:image" content={ogImage} />
+      <meta property="og:description" content={ogDescription || description} />
+      <meta property="og:site_name" content={ogSiteName} />
+      <meta property="og:locale" content="en_US" />
+
+      {/* Twitter Card */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={ogTitle || title} />
+      <meta name="twitter:description" content={ogDescription || description} />
+      <meta name="twitter:image" content={ogImage} />
+
+      {/* Additional SEO meta tags */}
+      <meta name="geo.region" content="ID-BA" />
+      <meta name="geo.placename" content="Seminyak, Bali" />
+      <meta name="geo.position" content="-8.676446;115.153198" />
+      <meta name="ICBM" content="-8.676446, 115.153198" />
+
+      {/* Favicon */}
+      <link rel="icon" type="image/png" href="/favicon.png" />
+      <link rel="apple-touch-icon" href="/icon.png" />
+
+      {/* Stylesheets */}
+      <link rel="stylesheet" href="/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="/css/normalize.css" />
+      <link rel="stylesheet" href="/css/swiper.min.css" />
+      <link rel="stylesheet" href="/css/aos.css" />
+      <link rel="stylesheet" href="/css/main.css" />
+    </Head>
+  );
+}
--- pages/faq.js
+++ pages/faq.js
@@ -3,6 +3,7 @@ import clsx from "clsx";
 import { useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
+import Footer from "../components/Footer";
 
 export default function Home() {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
@@ -793,52 +794,7 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="footer">
-          <div className="container">
-            <p>Re Massage Studio, JL. PETITENGET 88D, BALI, 80361, INDONESIA</p>
-            <p className="mb30">
-              {" "}
-              <a href="tel:+6282266698020" className="link-footer">
-                +62 822 6669 8020
-              </a>
-              <a href="mailto:care@remassagestudio.com" className="link-footer">
-                CARE@REMASSAGESTUDIO.COM
-              </a>
-            </p>
-            <ul className="social">
-              <li>
-                <a
-                  href="https://www.facebook.com/pages/Re-Day-Spa/821996157852429"
-                  target="_blank"
-                  className="social-link"
-                  rel="noreferrer"
-                >
-                  <img src="img/ico-facebook.svg" className="img-fluid" />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://twitter.com/ReDaySpa"
-                  className="social-link"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src="img/ico-twitter.svg" className="img-fluid" />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.instagram.com/remassagestudio/"
-                  className="social-link"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src="img/ico-instagram.svg" className="img-fluid" />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </section>
+        <Footer />
 
         <div className="button-toggle">
           <button
